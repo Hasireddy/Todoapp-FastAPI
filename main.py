a74b59ca5756bc1beb27ee5b0f97893bdbd29bde
Uma <malikireddy.uma@gmail.com>
2025-12-11 17:37:15 +0100

    Fetched all tasks using GET request

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 async def greet():
     return {"message":"Welcome"}
 
-my_list = [
+my_tasks = [
     Tasks(id=1,name="task1",status="completed"),
     Tasks(id=2,name="task2",status="pending"),
     Tasks(id=3,name="task3",status="pending"),
@@ -15,3 +15,22 @@ my_list = [
     Tasks(id=5,name="task5",status="pending"),
    
 ]
+
+
+#Get request to fetch the data
+
+@app.get("/tasks")
+
+def get_tasks():
+    return my_tasks
+
+
+#Get tasks by id
+
+@app.get("/task/{id}")
+
+async def get_task_by_id(id:int):
+    for task in my_tasks:
+        if task.id == id:
+            return task
+    return "Task not found"
