d0d7553f24cef5e0349eba9bd13212b1057a6e98
Uma <malikireddy.uma@gmail.com>
2025-12-19 09:31:44 +0100

    database

--- database_models.py
+++ database_models.py
@@ -3,8 +3,8 @@ from sqlalchemy import Column,Integer,String
 
 Base = declarative_base()
 
-class Task(Base):
+class TaskDB(Base):
     __tablename__ = "task"
-    id = Column(Integer,primary_key=True,Index=True)
+    id = Column(Integer,primary_key=True,index=True)
     name = Column(String)
     status = Column(String)
