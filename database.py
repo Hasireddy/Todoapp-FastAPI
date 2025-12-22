8c22f332f58c732d49976635a8047d7b90ca3273
abhi-110065 <ranjanabhijeet829107@gmail.com>
2025-12-22 18:12:53 +0530

    yield database instead of return

--- database.py
+++ database.py
@@ -1,23 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 
 # Engine: Connect to DB
 # Session: Open connection where we interact with DB
 # Base: Base class models for creating SQLAlchemy ORM
 # Model: Python Class -> DB Table
 
-#db_url = "sqlite:///./tasks.db"
-db_url = "postgresql://postgres:root@localhost:5432/tasks"
-engine = create_engine(db_url)
+db_url = "sqlite:///./tasks.db"
+# db_url = "postgresql://postgres:root@localhost:5432/tasks"
+engine = create_engine(db_url, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db():
+def get_db() -> Generator:
+    """
+    Dependency that provides a database session.
+
+    Yields a database session and ensures it's properly closed
+    after the request is complete.
+    """
     db = SessionLocal()
     try:
-        return db
+        yield db
     finally:
         db.close()
-
-get_db()
